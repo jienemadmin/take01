8fabcc59a00fcc1ffa9320d1c53588a45875cf09
jienemadmin <132441419+jienemadmin@users.noreply.github.com>
2026-03-04 17:58:43 +0900

    Create page.tsx

---
+++ app/me/page.tsx
@@ -0,0 +1,22 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function MePage() {
+
+  const session = await getServerSession();
+
+  if(!session){
+    redirect("/login");
+  }
+
+  return (
+    <main style={{padding:40}}>
+      <h1>내 계정</h1>
+
+      <p>이메일: {session.user?.email}</p>
+      <p>이름: {session.user?.name}</p>
+      <p>ID: {session.user?.id}</p>
+
+    </main>
+  );
+}
