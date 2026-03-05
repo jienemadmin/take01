8dd59ce883629ce6268536c20555fc4fb6a5c033
jienemadmin <132441419+jienemadmin@users.noreply.github.com>
2026-03-05 15:57:44 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,8 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/me/:path*"],
+  matcher: [
+    "/me/:path*",     // 마이페이지 보호
+    "/api/me/:path*", // API 보호
+  ],
 };
