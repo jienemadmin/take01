d31969cfaa32d9e299d77b5470f55cb69e862ea0
jienemadmin <132441419+jienemadmin@users.noreply.github.com>
2026-03-05 10:59:01 +0900

    Update page.tsx

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,22 +1,15 @@
 import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
+import { authOptions } from "@/lib/auth";
 
 export default async function MePage() {
-
-  const session = await getServerSession();
-
-  if(!session){
-    redirect("/login");
-  }
+  const session = await getServerSession(authOptions);
 
   return (
-    <main style={{padding:40}}>
-      <h1>마이페이지</h1>
-
-      <p>이메일: {session.user?.email}</p>
-      <p>이름: {session.user?.name}</p>
-      <p>ID: {session.user?.id}</p>
-
+    <main style={{ padding: 40 }}>
+      <h1>마이페이: /me</h1>
+      <p>이메일: {session?.user?.email}</p>
+      <p>이름: {session?.user?.name}</p>
+      <p>ID: {(session?.user as any)?.id}</p>
     </main>
   );
 }
